1bf8209e33c81268abb05249a11c1d356123f304
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-10-02 21:46:37 +0600

    Added: coin details

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import CoinDetails from '../CoinDetails'
 import './Header.css'
 
 export default function Header() {
@@ -9,6 +10,9 @@ export default function Header() {
             </div>
             <nav>
                 <ul>
+                    <li>
+                        <CoinDetails />
+                    </li>
                     <li>
                         <Link to="/">HOME</Link>
                     </li>
